93a25f087361cf7e6fb682fd6236a2b1df8ab309
amitsk <amitsk2014@gmail.com>
2022-11-20 00:39:34 -0500

    added on page load

--- src/tuiter/PostSummaryList/PostSummaryList.js
+++ src/tuiter/PostSummaryList/PostSummaryList.js
@@ -1,12 +1,18 @@
 import PostSummaryListItem from "./PostSummaryListItem.js";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {findTuitsThunk} from "../../services/tuits-thunks";
 const PostSummaryList = () =>{
     //const Posts = useSelector(state => state.tuits)
     const {tuits} = useSelector(state => {
         console.log("posts are ",state.tuitsData)
         return state.tuitsData
-    })
-    //console.log("posts are ",Posts)
+    });
+    const dispatch = useDispatch();
+    useEffect(() =>{
+        dispatch(findTuitsThunk());
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]);
     return (
     <ul className="list-group">
         {
